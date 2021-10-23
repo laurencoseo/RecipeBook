00a028d43e593f4d4c06b5abbe94c5df85a3eea8
Lauren Coseo <laurencoseo@gmail.com>
2021-10-23 09:37:54 -0700

    create files

---
+++ RecipeBook/RecipeBook.cpp
@@ -0,0 +1,8 @@
+//
+//  RecipeBook.cpp
+//  RecipeBook
+//
+//  Created by Lauren Coseo on 10/23/21.
+//
+
+#include "RecipeBook.hpp"
---
+++ RecipeBook/RecipeBook.hpp
@@ -0,0 +1,13 @@
+//
+//  RecipeBook.hpp
+//  RecipeBook
+//
+//  Created by Lauren Coseo on 10/23/21.
+//
+
+#ifndef RecipeBook_hpp
+#define RecipeBook_hpp
+
+#include <stdio.h>
+
+#endif /* RecipeBook_hpp */
