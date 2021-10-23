cbd923da5b4cfeed34349c723d808333237a32a3
Lauren Coseo <laurencoseo@gmail.com>
2021-10-23 09:11:25 -0700

    Initial Commit

---
+++ RecipeBook/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  RecipeBook
+//
+//  Created by Lauren Coseo on 10/23/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
